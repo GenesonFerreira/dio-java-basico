77e72d5594cbed5de153e2b9dc7bd3561a34837b
GenesonFerreira <jgeneson@gmail.com>
2024-05-23 16:54:35 -0300

    Estrutura usando o Switch Case com e sem Break

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,19 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "B";
+
+        switch (plano) {
+            case "T":{
+                System.out.println("5GB para Youtube");
+            }
+            case "M":{
+                System.out.println("WhatsApp e Instagram grátis");
+            }
+            case "B":{
+                System.out.println("100 minutos de ligação");
+            }
+            default:
+                break;
+        }
+    }
+}
